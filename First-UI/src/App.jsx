68594dfece67a031359f6bd78c94beedb4b34cd2
Anurag <patranurag766557@gmail.com>
2026-06-03 17:08:24 +0530

    changed again

--- First-UI/src/App.jsx
+++ First-UI/src/App.jsx
@@ -1,9 +1,12 @@
 import react from 'react';
+import Section1 from './Components/Section1/Section1';
+import Section2 from './Components/Section2/Section2'
 
 const App = () => {
   return(
-    <div className='bg-red-600'>
-      App
+    <div>
+      <Section1/>
+      <Section2/>
     </div>
   )
 }
---
+++ First-UI/src/Components/Section1/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between w-full py-6 px-16 '>
+      <h4 className='bg-black text-white px-5 py-3 rounded-4xl uppercase'>Target Audience</h4>
+      <h5 className='bg-gray-100 rounded-4xl w-1/3 text-center h-1/3'>Digital banking Platfrom</h5>
+    </div>
+  )
+}
+
+export default Navbar
