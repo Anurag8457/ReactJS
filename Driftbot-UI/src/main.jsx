86e21369e3f3223786cc705810d85ffe4b66d77e
Anurag Patra <156587842+Anurag8457@users.noreply.github.com>
2026-06-05 16:09:37 +0530

    Update main.jsx

--- Driftbot-UI/src/main.jsx
+++ Driftbot-UI/src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import { ThemeProvider } from "@mui/material";
-import theme from "./theme/theme";
-
-import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
