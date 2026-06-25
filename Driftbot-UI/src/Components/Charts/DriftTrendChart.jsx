4b9642c914cb0b37b886aa9a4c36d9c2d3b513de
Anurag Patra <156587842+Anurag8457@users.noreply.github.com>
2026-06-25 19:38:59 +0530

    Update DriftTrendChart.jsx

--- Driftbot-UI/src/Components/Charts/DriftTrendChart.jsx
+++ Driftbot-UI/src/Components/Charts/DriftTrendChart.jsx
@@ -6,24 +6,27 @@ import {
   Tooltip,
   ResponsiveContainer
 } from "recharts";
+import { useState, useEffect } from "react";
+import { fetchDriftTrend } from '../../Services/DashboardApi';
 
-const data = [
-  { day: "Mon", drift: 3 },
-  { day: "Tue", drift: 5 },
-  { day: "Wed", drift: 4 },
-  { day: "Thu", drift: 7 },
-  { day: "Fri", drift: 2 },
-  { day: "Sat", drift: 6 },
-  { day: "Sun", drift: 3 }
-];
+// const data = [
+//   { day: "Mon", drift: 3 },
+//   { day: "Tue", drift: 5 },
+//   { day: "Wed", drift: 4 },
+//   { day: "Thu", drift: 7 },
+//   { day: "Fri", drift: 2 },
+//   { day: "Sat", drift: 6 },
+//   { day: "Sun", drift: 3 }
+// ];
 
-export default function DriftTrendChart() {
+
+export default function DriftTrendChart({driftData}) {
   return (
     <ResponsiveContainer
       width="100%"
       height={300}
     >
-      <BarChart data={data}>
+      <BarChart data={driftData}>
         <XAxis dataKey="day" />
         <YAxis />
         <Tooltip />
