6812410a5425181db3177445d411efc32f4d7cdc
Anurag Patra <156587842+Anurag8457@users.noreply.github.com>
2026-06-25 20:45:11 +0530

    Update EnvironmentsApi.jsx

--- Driftbot-UI/src/Services/EnvironmentsApi.jsx
+++ Driftbot-UI/src/Services/EnvironmentsApi.jsx
@@ -4,12 +4,10 @@ const API_BASE_URL = import.meta.env.VITE_API_PROD_URL;
 const CODE = import.meta.env.VITE_APP_CODE;
 const TENANT_ID = import.meta.env.VITE_TENANT_ID;
 const SUBSCRIPTION_ID = import.meta.env.VITE_SUBSCRIPTION_ID;
-const severityLevel = import.meta.env.VITE_SEVERITYLEVEL;
-const TimeRange = import.meta.env.VITE_TIMERANGE;
 
-export const fetchDriftReports = async () => {
+export const fetchEnvironments = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/api/reports/drifts/tenant`, {
+        const response = await axios.get(`${API_BASE_URL}/api/environments`, {
             params: {
                 code: CODE,
                 tenantId: TENANT_ID
@@ -24,28 +22,24 @@ export const fetchDriftReports = async () => {
     }
 };
 
-export const fetchDriftReportDetails = async () => {
+export const fetchEnvironmentSubscriptions= async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/api/reports/drifts/subscription/${SUBSCRIPTION_ID}`,
-            {
-                params: {
-                    code: CODE,
-                    tenantId: TENANT_ID,
-                    severityLevel: severityLevel,
-                    timeRange: TimeRange,
-                },
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+        const response = await axios.get(`${API_BASE_URL}/api/environments`, {
+             params: {
+                code: CODE,
+                subscriptionId: SUBSCRIPTION_ID
+            },
+            headers: {
+                'Content-Type': 'application/json'
             }
-        );
+        });
+        console.log("drift", response.data);
         return response.data;
     }
     catch (error) {
         console.error('Error fetching drift trend:', error);
     }
 };
-
 
 
 
