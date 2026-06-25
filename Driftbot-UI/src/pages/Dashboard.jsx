8524827fc281728195cef7ab4fa225b2fe13e74b
Anurag Patra <156587842+Anurag8457@users.noreply.github.com>
2026-06-25 20:01:40 +0530

    Update Dashboard.jsx

--- Driftbot-UI/src/pages/Dashboard.jsx
+++ Driftbot-UI/src/pages/Dashboard.jsx
@@ -1,23 +1,96 @@
 
 import KPICard from "../components/cards/KPICards";
-
-import DriftTrendChart from "../components/charts/DriftTrendChart";
-import SeverityChart from "../components/charts/SeverityChart";
-
+import Skeleton from "@mui/material/Skeleton"
+import DriftTrendChart from "../Components/Charts/DriftTrendChart";
+import SeverityChart from "../Components/Charts/SeverityChart";
+import CircularProgress from "@mui/material/CircularProgress";
 import CloudIcon from "@mui/icons-material/Cloud";
 import WarningIcon from "@mui/icons-material/Warning";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
-
+import { useState, useEffect } from "react";
 import { Card } from "@mui/material";
-<div className="bg-red-500 text-white p-5">
-  Tailwind Test
-</div>
+import axios from "axios";
+import { fetchOverview, fetchDriftTrend } from '../Services/DashboardApi';
+import { data } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import AiAssistantPopup from "./AiAssistantPopup";
+
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+  const [responseMsg, setResponseMsg] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [overview, setOverview] = useState([]);
+  const [driftTrend, setDriftTrend] = useState([]);
+  const [severityDistribution, setSeverityDistribution] = useState([]);
+  const [settings, setGovernanceSettings] = useState([]);
+
+  useEffect(() => {
+    async function fetchOverviewData() {
+      setLoading(true);
+      try {
+        const fetchedData = await fetchOverview();
+        console.log("overview", fetchedData);
+        setOverview(fetchedData.data);
+
+      } catch (error) {
+        console.error("Failed to fetch or process data", error);
+        setOverview([]);
+        fetchOverview(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    async function fetchDriftTrendData() {
+      try {
+        const data = await fetchDriftTrend();
+        console.log("data", data);
+
+        const severity = data?.data;
+
+        const severityDistributionArray = severity?.severityDistribution
+          ? Object.entries(severity.severityDistribution).map(([key, value]) => {
+            const keyStr = String(key || '');
+            return {
+              name: keyStr.charAt(0).toUpperCase() + keyStr.slice(1),
+              value,
+            };
+          })
+          : [];
+
+        console.log("sev", severityDistributionArray);
+        setSeverityDistribution(severityDistributionArray);
+
+        const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        const daygraph = data.data.driftVolume.map(item => {
+          const dateObj = new Date(item.date);
+          const day = dayNames[dateObj.getDay()]; // getDay() returns 0-6 (Sun to Sat)
+          return {
+            day: day,
+            drift: item.count,
+          };
+        });
+
+        setDriftTrend(daygraph);
+
+      } catch (error) {
+        console.error("Failed to fetch or process data", error);
+        setSeverityDistribution([]);
+        setDriftTrend([]);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchOverviewData();
+    fetchDriftTrendData();
+
+  }, []);
+
   return (
     <div>
 
-      <h1 className="text-4xl font-bold mb-8">
+      <h1 className="text-3xl font-bold mb-8">
         Executive Overview
       </h1>
 
@@ -32,34 +105,52 @@ export default function Dashboard() {
         gap-6
       "
       >
-        <KPICard
-          title="Managed Environments"
-          value="24"
-          icon={<CloudIcon />}
-          color="#FFE9DD"
-        />
+        <Link to="/environment">
+          <KPICard
+            title="Managed Environments"
+            value={overview ? overview.healthScore : ''}
+            icon={<CloudIcon />}
+            color="#FFE9DD"
+          />
+        </Link>
 
-        <KPICard
-          title="Active Drift Issues"
-          value="7"
-          icon={<WarningIcon />}
-          color="#FFE2E2"
-        />
+        <Link to="/reports">
+          <KPICard
+            title="Active Drift Issues"
+            value={overview ? overview.activeDrifts : ''}
+            icon={<WarningIcon />}
+            color="#FFE2E2"
+          />
+        </Link>
 
-        <KPICard
-          title="Compliance Score"
-          value="85%"
-          icon={<CheckCircleIcon />}
-          color="#E8FFF0"
-        />
+        <Link to="/governance">
+          <KPICard
+            title="Compliance Score"
+            value={overview ? overview.complianceScore + "%" : ''}
+            icon={<CheckCircleIcon />}
+            color="#E8FFF0"
+          />
+        </Link>
 
-        <KPICard
-          title="Mean Resolution Time"
-          value="4.2h"
-          icon={<AccessTimeIcon />}
-          color="#E6F0FF"
-        />
+        <Link to="/ai-insights">
+          <KPICard
+            title="Mean Resolution Time"
+            value={overview ? overview.avgResolutionTimeHours : ''}
+            icon={<AccessTimeIcon />}
+            color="#E6F0FF"
+          />
+        </Link>
       </div>
+
+      {/* Loading Overlay */}
+      {loading && (
+        <div className="h-350px flex justify-center items-center" >
+          <CircularProgress sx={{
+            color: '#FF530D', // your custom orange color
+          }}
+          />
+        </div>
+      )}
 
       {/* CHARTS */}
 
@@ -80,7 +171,7 @@ export default function Dashboard() {
             Drift Trend
           </h2>
 
-          <DriftTrendChart />
+          <DriftTrendChart driftData={driftTrend} />
         </Card>
 
         <Card
@@ -90,8 +181,9 @@ export default function Dashboard() {
             Severity Distribution
           </h2>
 
-          <SeverityChart />
+          <SeverityChart severityData={severityDistribution} />
         </Card>
+        <AiAssistantPopup />
 
       </div>
 
